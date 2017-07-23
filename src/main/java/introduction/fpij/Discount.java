f076bb81271cd36af8ee02115ff1384a78fa0940
Joaquin Caro <me@joaquin-caro.es>
2017-07-23 17:19:52 +0200

    get total of Discounted Prices in functional way

--- src/main/java/introduction/fpij/Discount.java
+++ src/main/java/introduction/fpij/Discount.java
@@ -5,19 +5,33 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Discount {
+    public static final List<BigDecimal> PRICES = Arrays.asList(
+            new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
+            new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
+            new BigDecimal("45"), new BigDecimal("12"));
+
     public static void main(String[] args) {
         imperativeWay();
+        functionalWay();
+    }
+
+    private static void functionalWay() {
+        System.out.println("Functional: ");
+        final BigDecimal totalOfDiscountPrices =
+                PRICES.stream()
+                .filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0 )
+                .map(price -> price.multiply(BigDecimal.valueOf(0.9)))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        System.out.println("Total of discount prices: " + totalOfDiscountPrices);
     }
 
     private static void imperativeWay() {
-        final List<BigDecimal> prices = Arrays.asList(
-                new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
-                new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
-                new BigDecimal("45"), new BigDecimal("12"));
+        System.out.println("Imperative: ");
 
         BigDecimal totalOfDiscountPrices = BigDecimal.ZERO;
 
-        for(BigDecimal price : prices) {
+        for(BigDecimal price : PRICES) {
             if(price.compareTo(BigDecimal.valueOf(20)) > 0)
                 totalOfDiscountPrices = totalOfDiscountPrices.add(price.multiply(BigDecimal.valueOf(0.9)));
         }
