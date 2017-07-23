b34fac382715d432b7a9a98b9c57ce3b1fe4a3ec
Joaquin Caro <me@joaquin-caro.es>
2017-07-23 16:55:06 +0200

    Declarative search

--- src/main/java/introduction/fpij/Cities.java
+++ src/main/java/introduction/fpij/Cities.java
@@ -4,15 +4,24 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Cities {
+
+    public static final List<String> CITIES = Arrays.asList("Madrid", "Chicago", "Barcelona", "London", "New York");
+
     public static void main(String[] args) {
         imperativeSearch();
+        declarativeSearch();
+    }
+
+    private static void declarativeSearch() {
+        System.out.println("Declarative: ");
+        System.out.println("Found chicago? " + CITIES.contains("Chicago"));
     }
 
     private static void imperativeSearch() {
+        System.out.println("Imperative: ");
         boolean found = false;
-        List<String> cities = Arrays.asList("Madrid", "Chicago", "Barcelona", "London", "New York");
 
-        for (String city : cities) {
+        for (String city : CITIES) {
             if(city.equals("Chicago")) {
                 found = true;
                 break;
